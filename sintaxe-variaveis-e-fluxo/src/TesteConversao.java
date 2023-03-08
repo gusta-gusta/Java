dd434d0a7f2fe52414a2b608965b270cd769310d
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2023-03-08 11:30:27 -0300

    Aula 4
    att

---
+++ sintaxe-variaveis-e-fluxo/src/TesteConversao.java
@@ -0,0 +1,15 @@
+
+public class TesteConversao {
+
+		public static void main(String[] args) {
+			
+			double salario = 15237.20;
+			
+			System.out.println(salario);
+			
+			int valor = (int) salario;
+			
+			System.out.println(valor);
+			
+		}
+}
