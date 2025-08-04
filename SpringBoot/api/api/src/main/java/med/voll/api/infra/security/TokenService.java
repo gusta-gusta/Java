51507e420c00d502e82cb81edbcd11bfe93e1ff5
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2025-08-04 11:50:23 -0300

    att
    att 08

--- SpringBoot/api/api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ SpringBoot/api/api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,7 +4,9 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -15,10 +17,13 @@ import med.voll.api.domain.usuario.Usuario;
 @Service
 public class TokenService {
 
+	@Value("${api.security.token.secret}")
+	private String secret;
+	
 	public String gerarToken(Usuario usuario) {
 		
 		try {
-		    var algoritmo = Algorithm.HMAC256("12345678");
+		    var algoritmo = Algorithm.HMAC256(secret);
 		   return JWT.create()
 		        .withIssuer("API Voll.med")
 		        .withSubject(usuario.getUsername())
