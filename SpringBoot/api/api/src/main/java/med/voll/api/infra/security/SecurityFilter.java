beb54aaf09e38a2ea113a7c54a558d6889d92ae5
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2025-08-12 11:49:28 -0300

    token
    eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJndXN0YXZvX3NhbGVzMjAxNEBvdXRsb29rLmNvbSIsImlzcyI6IkFQSSBWb2xsLm1lZCIsImV4cCI6MTc1NDkyMTQ4Nn0.CKVFRwMz51Oj-pAFBGrXwlg5mI20Y4fM_QPnmC3NW80

--- SpringBoot/api/api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ SpringBoot/api/api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -1,21 +1,37 @@
 package med.voll.api.infra.security;
 
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        
+    
+    	var tokenJWT = recuperarToken(request);
+    	
+    	
+    	System.out.println("Token: " + tokenJWT);
+    	
     	// FilterChain - Representa a Cadeia de Filtro da Aplicação, necessário para chamar os próximos filtros
     	filterChain.doFilter(request, response);
+    }
+    
+    private Object recuperarToken(HttpServletRequest request) {
+    	var authorizationHeader = request.getHeader("Authorization");
+    	if (authorizationHeader == null) {
+    		throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+    	}
+    	
+    	// Removendo o prefixo Beaer
+    	return authorizationHeader.replace("Bearer ", "");
     }
 }
