5324175ed244e3b7b0b3d9c24579ebda5e4a98a2
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2025-08-13 11:56:49 -0300

    att
    att

--- SpringBoot/api/api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ SpringBoot/api/api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -2,6 +2,7 @@ package med.voll.api.infra.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,12 +13,17 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+	
+	@Autowired
+	private TokenService tokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
     
     	var tokenJWT = recuperarToken(request);
     	
+    	var subject = tokenService.getSubject(tokenJWT);
+    	System.out.println(subject);
     	
     	System.out.println("Token: " + tokenJWT);
     	
@@ -25,7 +31,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     	filterChain.doFilter(request, response);
     }
     
-    private Object recuperarToken(HttpServletRequest request) {
+    private String recuperarToken(HttpServletRequest request) {
     	var authorizationHeader = request.getHeader("Authorization");
     	if (authorizationHeader == null) {
     		throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
--- SpringBoot/api/api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ SpringBoot/api/api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,13 +4,14 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.context.annotation.ApplicationScope;
-
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import med.voll.api.domain.usuario.Usuario;
 
@@ -19,27 +20,36 @@ public class TokenService {
 
 	@Value("${api.security.token.secret}")
 	private String secret;
-	
+
 	public String gerarToken(Usuario usuario) {
-		
+
 		try {
-		    var algoritmo = Algorithm.HMAC256(secret);
-		   return JWT.create()
-		        .withIssuer("API Voll.med")
-		        .withSubject(usuario.getUsername())
-		        .withExpiresAt(dataExpiracao())
-		        .sign(algoritmo);
-		} catch (JWTCreationException exception){
+			var algoritmo = Algorithm.HMAC256(secret);
+			return JWT.create().withIssuer("API Voll.med").withSubject(usuario.getUsername())
+					.withExpiresAt(dataExpiracao()).sign(algoritmo);
+		} catch (JWTCreationException exception) {
 			throw new RuntimeException("Erro na geração do token jwt", exception);
 		}
-		
+
+	}
+
+	public String getSubject(String tokenJWT) {
+		try {
+			var algoritmo = Algorithm.HMAC256(secret);
+			return JWT.require(algoritmo)
+					.withIssuer("API Voll.med")
+					.build()
+					.verify(tokenJWT)
+					.getSubject();
+			
+		} catch (JWTVerificationException exception) {
+			throw new RuntimeException("Token jwt inváldo ou expirado!");
+		}
 	}
 
 	private Instant dataExpiracao() {
-		
+
 		return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
 	}
-	
-	
-	
+
 }
